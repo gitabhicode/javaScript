f0eca132256b0c9f944d44ac864e21715698565f
gitabhicode <yadavabhishek29200@gmail.com>
2025-06-13 19:16:26 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score =  "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true = 1 ; flase => 0;
+
+let isLoggedIn = "Abhishek yadav"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//  1 => true ; 0=>flase
+// " " => false
+// "abhishek" => true
+
+let a = 33
+let stringA = String(a);
+console.log(stringA); 
+console.log(typeof stringA); 
+
