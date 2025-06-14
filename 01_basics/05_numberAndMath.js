0b6bf227369a399952a9d47699f7555299c47589
gitabhicode <yadavabhishek29200@gmail.com>
2025-06-14 09:05:31 +0000

    math and num

---
+++ 01_basics/05_numberAndMath.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(2)); 
+
+const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); 
+
+
+
+//******************Maths******************** */
+
+ console.log(Math)
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.335));
+// console.log(Math.ceil(4.335));
+// console.log(Math.floor(4.335));
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+
+console.log(Math.random()); // gives value bw 0 ad 1
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 60;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
